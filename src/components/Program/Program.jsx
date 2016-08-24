2dd33b7141958afe5789cb1857d0bec732c71890
roberth26 <robbiehall26@gmail.com>
2016-08-24 01:19:08 -0700

    program styles

--- src/components/Program/Program.jsx
+++ src/components/Program/Program.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './Program.scss';
+import format_number from 'format-number';
 
 export default class Program extends React.Component {
 	constructor( props ) {
@@ -18,6 +19,7 @@ export default class Program extends React.Component {
 	}
 
 	render() {
+		console.log( this.props.program );
 		let edit_btn_styles = {
 			backgroundImage: 'url( images/pencil_icons.png )'
 		};
@@ -34,11 +36,11 @@ export default class Program extends React.Component {
 							<div className="program__totals__title">Total Monthly</div>
 							<div className="program__totals__cell">Sales</div>
 						</div>
-						<div className="program__totals__col col-3">
-							<div className="program__totals__title">Current</div>
-							<div className="program__totals__cell program__totals__cell--dark">$$$</div>
-						</div>
 						<div className="program__totals__col col-4">
+							<div className="program__totals__title">Current</div>
+							<div className="program__totals__cell program__totals__cell--dark">${format_number()( this.props.program.TotalMonthlySales )}</div>
+						</div>
+						<div className="program__totals__col col-3">
 							<div className="program__totals__title program__totals__title--centered">1-Year</div>
 							<div className="program__totals__cell">
 								<img className="program__totals__graph" src="images/spark_line.png"
@@ -49,7 +51,7 @@ export default class Program extends React.Component {
 				</div>
 				<div className="program__collapse">
 					<div className="program__pricing-options">
-						<div className="program__pricing-options__row">
+						<div className="program__pricing-options__row program__pricing-options__row--headings">
 							<div className="program__pricing-options__col col-5">
 								<div className="program__pricing-options__title">Price Name</div>
 							</div>
@@ -62,17 +64,20 @@ export default class Program extends React.Component {
 						</div>
 						{this.props.program.pricing_options.map( ( po, i ) => {
 							return (
-								<div className="program__pricing-options__row">
+								<div className="program__pricing-options__row" key={ i }>
 									<div className="program__pricing-options__col col-5">
 										<div className="program__pricing-options__cell program__pricing-options__cell--dark">{ po.Name }</div>
 									</div>
 									<div className="program__pricing-options__col col-3">
-										<div className="program__pricing-options__cell">$$$</div>
+										<div className="program__pricing-options__cell">${ format_number()( po.Sales ) }</div>
 									</div>
 									<div className="program__pricing-options__col col-4">
 										<div className="program__pricing-options__cell">
-											<img className="program__pricing-options__graph" src="simages/spark_lines.png"
-												alt={ this.props.program.Name + ' 1-year' } />
+											<div className="program__pricing-options__graph-wrapper">
+												<img className={ 'program__pricing-options__graph ' + 'program__pricing-options__graph--0' + Math.round( ( Math.random() * 6 ) ) }
+													src="images/spark_lines.png"
+													alt={ this.props.program.Name + ' 1-year' } />
+											</div>
 										</div>
 									</div>
 								</div>
